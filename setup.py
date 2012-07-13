bd8df2508791f8a4e6cedec259c3dcc823f56d6c
Alexander Todorov <atodorov@redhat.com>
2012-07-13 22:44:19 +0300

    cut v0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name = 'django-s3-cache',
-    version = '0.1',
+    version = '0.2',
     packages = find_packages(),
     author = 'Alexander Todorov',
     author_email = 'atodorov@nospam.otb.bg',
