bf7ed206635bcd0cd0243b6375272529848f8741
Alexander Todorov <atodorov@redhat.com>
2013-05-30 01:52:15 +0300

    bump to v1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as file:
 
 setup(
     name = 'django-s3-cache',
-    version = '1.1',
+    version = '1.2',
     packages = find_packages(),
     author = 'Alexander Todorov',
     author_email = 'atodorov@nospam.otb.bg',
