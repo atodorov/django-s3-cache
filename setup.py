b4176ffaf9b204ac7257b952e51be90bc56a38c6
Alexander Todorov <atodorov@redhat.com>
2014-12-03 23:09:48 +0200

    add class to run tests

--- setup.py
+++ setup.py
@@ -2,6 +2,26 @@
 
 import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 with open('README.rst') as file:
     long_description = file.read()
@@ -28,6 +48,7 @@ config = {
     ],
     'zip_safe' : False,
     'install_requires' : ['boto','django-storages>=1.1.8','Django'],
+    'cmdclass' : {'test': PyTest},
 }
 
 if (len(sys.argv) >= 2) and (sys.argv[1] == '--requires'):
