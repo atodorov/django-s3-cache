2d78562ebedb5c789e9e46c348a6152e33130a66
Alexander Todorov <atodorov@redhat.com>
2014-12-03 18:12:21 +0200

    refactor setup.py so that it can print install_requires, needed for testing

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
+import sys
 from setuptools import setup, find_packages
 
 with open('README.rst') as file:
     long_description = file.read()
 
-setup(
-    name = 'django-s3-cache',
-    version = '1.4',
-    packages = find_packages(),
-    author = 'Alexander Todorov',
-    author_email = 'atodorov@nospam.otb.bg',
-    license = 'BSD',
-    description = 'Amazon Simple Storage Service (S3) cache backend for Django',
-    long_description = long_description,
-    url='https://github.com/atodorov/django-s3-cache',
-    keywords = ['Amazon', 'S3', 'Django', 'cache'],
-    classifiers = [
+config = {
+    'name' : 'django-s3-cache',
+    'version' : '1.4',
+    'packages' : find_packages(),
+    'author' : 'Alexander Todorov',
+    'author_email' : 'atodorov@nospam.otb.bg',
+    'license' : 'BSD',
+    'description' : 'Amazon Simple Storage Service (S3) cache backend for Django',
+    'long_description' : long_description,
+    'url' : 'https://github.com/atodorov/django-s3-cache',
+    'keywords' : ['Amazon', 'S3', 'Django', 'cache'],
+    'classifiers' : [
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -25,6 +26,12 @@ setup(
         'Programming Language :: Python',
         'Framework :: Django',
     ],
-    zip_safe = False,
-    install_requires=['boto','django-storages>=1.1.8','Django']
-)
+    'zip_safe' : False,
+    'install_requires' : ['boto','django-storages>=1.1.8','Django'],
+}
+
+if (len(sys.argv) >= 2) and (sys.argv[1] == '--requires'):
+    for req in config['install_requires']:
+        print req
+else:
+    setup(**config)
