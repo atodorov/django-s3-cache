b76a9c50b95c56614eb36a0dafc0ad9cb0f10f24
Alexander Todorov <atodorov@redhat.com>
2014-12-04 02:15:14 +0200

    version 1.4.2 with working tests under Django 1.7

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ with open('README.rst') as file:
 
 config = {
     'name' : 'django-s3-cache',
-    'version' : '1.4.1',
+    'version' : '1.4.2',
     'packages' : find_packages(),
     'author' : 'Alexander Todorov',
     'author_email' : 'atodorov@nospam.otb.bg',
